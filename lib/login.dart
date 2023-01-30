4bd1504b2bc2396e5dd6094297d9e6d1d6b1c9b1
Thiago Lima <thiago@airtm.io>
2023-01-30 22:54:55 +0000

    refactor login page

--- lib/login.dart
+++ lib/login.dart
@@ -8,50 +8,82 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Color(0xCCDE28FF),
-            Color(0xC44A1DFF),
-          ],
-        )),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Center(
-                child: SizedBox(
-                  height: 100,
-                  width: 100,
-                  child: Placeholder(
-                    color: Colors.amberAccent,
+      backgroundColor: Colors.white,
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Stack(
+              fit: StackFit.expand,
+              alignment: AlignmentDirectional.bottomCenter,
+              children: [
+                const Image(
+                  fit: BoxFit.fitHeight,
+                  image: AssetImage('assets/images/login_house.jpg'),
+                ),
+                Positioned(
+                  bottom: 0,
+                  child: SignInButton(
+                    Buttons.Google,
+                    text: 'Entrar com o Google',
+                    onPressed: _signInWithGoogle,
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              const Text(
-                'Casa Verão 2023',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.amberAccent,
-                ),
-              ),
-              const SizedBox(
-                height: 36,
-              ),
-              SignInButton(
-                Buttons.Google,
-                text: 'Entrar com o Google',
-                onPressed: _signInWithGoogle,
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text.rich(
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black54,
+                    ),
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "ATENÇÃO: ",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text:
+                              "Caso não tenha um gmail, convidamos você a conhecer essa ferramenta inovadora que foi lançada recentemente, por volta de 2004. ",
+                        ),
+                        TextSpan(
+                          text: "\nWelcome to the future",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  constraints: BoxConstraints.tight(const Size.square(250)),
+                  child: Image.asset('assets/images/logo.png'),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    '© Copyright 2023 - Abestados Produções S.A.',
+                    style: TextStyle(
+                      color: Colors.black87,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
